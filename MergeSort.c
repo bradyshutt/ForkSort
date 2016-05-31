799fe92a1bd4b8b02353e18d8b5ff92222a624b4
Brady <bradyshutt@gmail.com>
2016-05-31 07:22:37 -0700

    Tested mergesort function

--- MergeSort.c
+++ MergeSort.c
@@ -146,74 +146,36 @@ Set *MergeSort(Set *set) {
 
    merged = Merge(leftPartition, rightPartition);
 
-   PrintState("Merged", merged);
+   //PrintState("Merged", merged);
 
    return merged;
 
 }
 
 int main(int argc, char *argv[]) {
-   int idx = 0;
-   Set *set = NewSet(20);
+   int idx;
+   Set *random;
 
    srand(time(NULL));
 
-   //   set->elems[0] = 0;
-   //   set->elems[1] = 2;
-   //   set->elems[2] = 4;
-   //   set->elems[3] = 6;
-   //   set->elems[4] = 8;
-   //   set->elems[5] = 10;
-   //   set->elems[6] = 12;
-   //   set->elems[7] = 14;
-   //   set->elems[8] = 16;
-   //   set->elems[9] = 18;
-   //
-   //   set->elems[10] = 1;
-   //   set->elems[11] = 3;
-   //   set->elems[12] = 5;
-   //   set->elems[13] = 7;
-   //   set->elems[14] = 9;
-   //   set->elems[15] = 11;
-   //   set->elems[16] = 13;
-   //   set->elems[17] = 15;
-   //   set->elems[18] = 17;
-   //   set->elems[19] = 19;
+   int val;
+   for (idx = 0; idx < 1000000; idx++) {
 
-   set->elems[idx++] = 5;
-   set->elems[idx++] = 2;
-   set->elems[idx++] = 1;
-   set->elems[idx++] = 7;
-   set->elems[idx++] = 14;
-   set->elems[idx++] = 13;
-   set->elems[idx++] = 19;
-   set->elems[idx++] = 16;
-   set->elems[idx++] = 6;
-   set->elems[idx++] = 0;
-   set->elems[idx++] = 12;
-   set->elems[idx++] = 17;
-   set->elems[idx++] = 9;
-   set->elems[idx++] = 4;
-   set->elems[idx++] = 15;
-   set->elems[idx++] = 8;
-   set->elems[idx++] = 11;
-   set->elems[idx++] = 3;
-   set->elems[idx++] = 18;
-   set->elems[idx++] = 10;
+      val = (rand() % 50) + 1;
+      //fprintf(stderr, "rand val: %d\n", val);
+      random = NewRandomSet(val);
+      //PrintState("Unsorted", random);
+      random = MergeSort(random);
+      //PrintState("Sorted", random);
+      if (IsSorted(random)) {
+         fprintf(stderr, "%s", ".");
+      }
+      else  {
+         fprintf(stderr, "%s", "ERROR");
+         break;
+      }
+   }
 
-   PrintState("initial set", set);
-
-   Set *rand = NewRandomSet(18);
-   PrintState("rand set", rand);
-
-   set = MergeSort(set);
-   rand = MergeSort(rand);
-
-   PrintState("final set ", set);
-   PrintState("final rand", rand);
-
-   printf("Final Set is..... %s\n", IsSorted(set) ? "SORTED!": "not sorted :(");
-   printf("Final RND is..... %s\n", IsSorted(rand) ? "SORTED!": "not sorted :(");
 
    return 0;
 }
